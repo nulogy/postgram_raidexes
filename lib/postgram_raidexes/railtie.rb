798c9d31037af98698673c53a43c7b14039387be
Jason Schweier <jasons@nulogy.com>
2017-08-01 21:44:59 -0400

    only require extensions when using postgres

--- lib/postgram_raidexes/railtie.rb
+++ lib/postgram_raidexes/railtie.rb
@@ -1,7 +1,11 @@
 module PostgramRaidexes
   class Railtie < Rails::Railtie
     initializer "postgram_raidexes.postgresql_overrides" do
-      require "postgram_raidexes/pg_opclass_support"
+      require "postgram_raidexes/pg_opclass_support" if using_postgresql?
+    end
+
+    def using_postgresql?
+      ActiveRecord::Base.configurations[Rails.env]['adapter'].casecmp('postgresql').zero?
     end
   end
 end
