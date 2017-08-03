61f6500ec2969932c517e3025e1202f1aefc24ea
Jason Schweier <jasons@nulogy.com>
2017-08-02 20:58:56 -0400

    prep Railtie for Rails version checking

--- lib/postgram_raidexes/railtie.rb
+++ lib/postgram_raidexes/railtie.rb
@@ -1,7 +1,9 @@
 module PostgramRaidexes
   class Railtie < Rails::Railtie
     initializer "postgram_raidexes.postgresql_overrides" do
-      require "postgram_raidexes/pg_opclass_support" if using_postgresql?
+      break unless using_postgresql?
+
+      require "postgram_raidexes/pg_opclass_support"
     end
 
     def using_postgresql?
