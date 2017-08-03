6a0aa36e7be996a10c26d0a72fbb496c0d8d1c93
Jason Schweier <jasons@nulogy.com>
2017-08-02 21:26:44 -0400

    require specific file for Rails 4.2

--- lib/postgram_raidexes/railtie.rb
+++ lib/postgram_raidexes/railtie.rb
@@ -5,7 +5,10 @@ module PostgramRaidexes
 
       raise "Only Rails 4 is supported at this time" unless Rails::VERSION::MAJOR == 4
 
-      require "postgram_raidexes/pg_opclass_support"
+      case Rails::VERSION::MINOR
+      when 2
+        require "postgram_raidexes/pg_opclass_support_rails_42"
+      end
     end
 
     def using_postgresql?
