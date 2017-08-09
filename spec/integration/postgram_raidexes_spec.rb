e7d688290e608a80a7c641059f56da9a29b5d440
Jason Schweier <jasons@nulogy.com>
2017-08-08 21:48:34 -0400

    inlined let statements to local variables

--- spec/integration/postgram_raidexes_spec.rb
+++ spec/integration/postgram_raidexes_spec.rb
@@ -1,23 +1,18 @@
 require "spec_helper"
 
 RSpec.describe PostgramRaidexes do
-  let(:dumped_schema_lines) { test_app_schema_lines }
-  let(:users_email_trigram_index_dump) do
-    %q|add_index "users", ["email"], name: "index_users_on_email_trigram", using: :gin, opclasses: {"email"=>"gin_trgm_ops"}|
-  end
-  let(:users_last_name_trigram_index_dump) do
-    %q|add_index "users", ["last_name"], name: "index_users_on_last_name_trigram", using: :gin, opclasses: {"last_name"=>"gin_trgm_ops"}|
-  end
-
   before do
     migrate_test_app
   end
 
   it "dumps trigram indexes" do
+    users_email_index = 'add_index "users", ["email"], name: "index_users_on_email_trigram", using: :gin, opclasses: {"email"=>"gin_trgm_ops"}'
+    users_last_name_index = 'add_index "users", ["last_name"], name: "index_users_on_last_name_trigram", using: :gin, opclasses: {"last_name"=>"gin_trgm_ops"}'
+
     schema_lines = dumped_schema_lines_from_test_app
 
-    expect(schema_lines).to include_line(users_email_trigram_index_dump)
-    expect(schema_lines).to include_line(users_last_name_trigram_index_dump)
+    expect(schema_lines).to include_line(users_email_index)
+    expect(schema_lines).to include_line(users_last_name_index)
   end
 
   it "doesn't mangle partial indexes" do
@@ -32,10 +27,6 @@ RSpec.describe PostgramRaidexes do
     migrate
   end
 
-  def dumped_schema_lines_from_test_app
-    File.readlines("spec/support/test_app/db/schema.rb")
-  end
-
   def delete_old_schema_file
     test_schema_path = "spec/support/test_app/db/schema.rb"
     File.delete(test_schema_path) if File.exist?(test_schema_path)
@@ -47,6 +38,10 @@ RSpec.describe PostgramRaidexes do
 
   def migrate
     `cd spec/support/test_app && bundle exec rake db:migrate`
+  end
+
+  def dumped_schema_lines_from_test_app
+    File.readlines("spec/support/test_app/db/schema.rb")
   end
 
   RSpec::Matchers.define :include_line do |expected_line|
