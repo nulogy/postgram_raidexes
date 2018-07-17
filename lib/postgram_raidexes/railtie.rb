d5b61a4b6dffce19791cf85b888a9a06b27ef6c2
Ryan Mok <ryanm@nulogy.com>
2018-07-17 09:51:10 -0400

    Remove error message for Rails 5

--- lib/postgram_raidexes/railtie.rb
+++ lib/postgram_raidexes/railtie.rb
@@ -3,13 +3,20 @@ module PostgramRaidexes
     initializer "postgram_raidexes.postgresql_overrides" do
       break unless using_postgresql?
 
-      raise "Only Rails 4 is supported at this time" unless Rails::VERSION::MAJOR == 4
+      raise "Only Rails 4 and 5 are supported at this time" unless Rails::VERSION::MAJOR == 4 || Rails::VERSION::MAJOR == 5
 
-      case Rails::VERSION::MINOR
-      when 1
-        require "postgram_raidexes/pg_opclass_support_rails_41"
-      when 2
-        require "postgram_raidexes/pg_opclass_support_rails_42"
+      if Rails::VERSION::MAJOR == 4
+        case Rails::VERSION::MINOR
+        when 1
+          require "postgram_raidexes/pg_opclass_support_rails_41"
+        when 2
+          require "postgram_raidexes/pg_opclass_support_rails_42"
+        end
+      elsif Rails::VERSION::MAJOR == 5
+        case Rails::VERSION::MINOR
+        when 0
+          require "postgram_raidexes/pg_opclass_support_rails_42"
+        end
       end
     end
 
